818219560589afe238e40b811cb229b4585eea6e
byun618 <byun618@gmail.com>
2022-02-01 17:13:50 +0900

    asd

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
-import {QuoationService} from 'node-upbit'
-
-const quoationService = new QuoationService()
+import {VBBackTest} from './public/volatility-breakout.helper'
 
 const test = async () => {
-  const data = await quoationService.getDayCandles({
-    marketCoin: 'KRW-BTC',
-    count: 10,
-  })
-
-  console.log(data.map((d) => d.candle_date_time_kst))
+  VBBackTest()
 }
 
 test()
--- src/public/quoation.helper.ts
+++ src/public/quoation.helper.ts
@@ -8,10 +8,48 @@ interface IGetDayCandles {
 }
 
 export const getDayCandles = async ({ticker, count}: IGetDayCandles) => {
-  const data = await quoationService.getDayCandles({
+  const candles = await quoationService.getDayCandles({
     marketCoin: ticker,
     count,
   })
 
-  return data
+  return candles
+}
+
+export const getMarkets = async (currency: string) => {
+  const markets: any = await quoationService.getMarketAllInfo()
+  return markets[currency].map((market: any) => market.market)
+}
+
+const getTicker = async (market: string) => {
+  const ticker = await quoationService.getTicker([market])
+  return ticker[0]
+}
+
+export const getTop30Markets = async () => {
+  const markets = await getMarkets('KRW')
+  const tickers: any = []
+
+  // await Promise.all(
+  //   markets.map(async (market: any) => {
+  //     const ticker = await getTicker(market)
+  //     tickers.push(ticker)
+  //   }),
+  // )
+
+  for (const market of markets) {
+    const ticker = await getTicker(market)
+    tickers.push(ticker)
+  }
+
+  const out = tickers
+    .sort((a: any, b: any) => {
+      const aValue = a.acc_trade_price
+      const bValue = b.acc_trade_price
+      return bValue - aValue
+    })
+    .slice(0, 29)
+    .map((ticker: any) => ticker.market)
+
+  return out
 }
