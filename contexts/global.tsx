c8d0e75db64e4dc35eadb919cec64e98820343a5
byun618 <byun618@gmail.com>
2022-03-17 07:05:11 +0900

    global context 구현

---
+++ contexts/global.tsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import axios from 'axios'
+
+const defaultValue: {
+  tickers: string[] | null
+} = {
+  tickers: null,
+}
+
+const GlobalContext = createContext(defaultValue)
+
+const GlobalProvider: React.FC = ({ children }) => {
+  const [tickers, setTickers] = useState<string[] | null>(null)
+
+  const fetchTickers = async () => {
+    const { data } = await axios.get('http://localhost:3001/tickers')
+
+    setTickers(data)
+  }
+
+  useEffect(() => {
+    fetchTickers()
+  }, [])
+
+  return (
+    <GlobalContext.Provider value={{ tickers }}>
+      {children}
+    </GlobalContext.Provider>
+  )
+}
+
+export const useGlobal = () => useContext(GlobalContext)
+
+export default GlobalProvider
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,19 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import SocketProvider from '../contexts/socket'
+import GlobalProvider from '../contexts/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <SocketProvider>
-        <Head>
-          <title>🤖희잉🤖</title>
-        </Head>
-        <Component {...pageProps} />
-      </SocketProvider>
+      <GlobalProvider>
+        <SocketProvider>
+          <Head>
+            <title>🤖희잉🤖</title>
+          </Head>
+          <Component {...pageProps} />
+        </SocketProvider>
+      </GlobalProvider>
     </>
   )
 }
