f84481867448bdbfa0c54a6dff1a2d53bb54b30b
byun618 <byun618@gmail.com>
2022-04-03 09:27:32 +0900

    코인 페이지 검색 UI 구현

--- components/Coin/Coin.tsx
+++ components/Coin/Coin.tsx
@@ -1,5 +1,35 @@
+import styled from '@emotion/styled'
+import { ChangeEvent, useState } from 'react'
+import SearchInput from './SearchInput'
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
 const Coin = () => {
-  return <>코인</>
+  const [search, setSearch] = useState<string>('')
+
+  const onChangeSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value)
+  }
+
+  const onClickSearch = () => {
+    alert(search)
+  }
+
+  return (
+    <Wrapper>
+      <SearchInput
+        type="text"
+        name="search"
+        value={search}
+        placeholder="코인명 / 티커"
+        onChange={onChangeSearch}
+        onClickSearch={onClickSearch}
+      />
+    </Wrapper>
+  )
 }
 
 export default Coin
